61750987c97e147bd3c1d7b1f8b44ee711d6c355
Flor Cerdan León <cerdanflor@gmail.com>
2021-01-28 14:35:16 -0500

    Semana 14 - Creación de NuevaNotaDialogFragment

---
+++ app/src/main/java/com/dam/prueba/NuevaNotaDialogFragment.java
@@ -0,0 +1,59 @@
+package com.dam.prueba;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NuevaNotaDialogFragment extends DialogFragment {
+
+    private NuevaNotaDialogViewModel mViewModel;
+
+    public static NuevaNotaDialogFragment newInstance() {
+        return new NuevaNotaDialogFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.nueva_nota_dialog_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(NuevaNotaDialogViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Nueva Nota");
+        builder.setMessage("Introduzca los datos de la nueva nota")
+                .setPositiveButton("Guardar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+}
---
+++ app/src/main/java/com/dam/prueba/NuevaNotaDialogViewModel.java
@@ -0,0 +1,7 @@
+package com.dam.prueba;
+
+import androidx.lifecycle.ViewModel;
+
+public class NuevaNotaDialogViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
