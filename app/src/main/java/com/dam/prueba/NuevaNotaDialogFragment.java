09edac428765e7c010d7d5dabd6e74567c4dd5a2
Flor Cerdan León <cerdanflor@gmail.com>
2021-01-28 16:04:26 -0500

    Semana 14 - DialogFragment (Cuadro de Dialogo) Listo

--- app/src/main/java/com/dam/prueba/NuevaNotaDialogFragment.java
+++ app/src/main/java/com/dam/prueba/NuevaNotaDialogFragment.java
@@ -14,20 +14,20 @@ import androidx.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 
 public class NuevaNotaDialogFragment extends DialogFragment {
 
     private NuevaNotaDialogViewModel mViewModel;
     private View view;
+    private EditText etTitulo, etContenido;
+    private RadioGroup rgColor;
+    private Switch swNotaFavorita;
 
     public static NuevaNotaDialogFragment newInstance() {
         return new NuevaNotaDialogFragment();
-    }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.nueva_nota_dialog_fragment, container, false);
     }
 
     @Override
@@ -45,7 +45,19 @@ public class NuevaNotaDialogFragment extends DialogFragment {
         builder.setMessage("Introduzca los datos de la nueva nota")
                 .setPositiveButton("Guardar", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        // FIRE ZE MISSILES!
+                        String titulo = etTitulo.getText().toString();
+                        String contenido = etContenido.getText().toString();
+                        String color = "azul";
+                        switch (rgColor.getCheckedRadioButtonId()){
+                            case R.id.radioButtonColorRojo:
+                                color = "rojo";
+                                break;
+                            case R.id.radioButtonColorVerde:
+                                color = "verde";
+                                break;
+                        }
+                        boolean esFavorita = swNotaFavorita.isChecked();
+
                     }
                 })
                 .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
@@ -55,6 +67,12 @@ public class NuevaNotaDialogFragment extends DialogFragment {
                 });
         LayoutInflater inflater = getActivity().getLayoutInflater();
         view = inflater.inflate(R.layout.nueva_nota_dialog_fragment,null);
+
+        etTitulo = view.findViewById(R.id.editTextTitulo);
+        etContenido = view.findViewById(R.id.editTextContenido);
+        rgColor = view.findViewById(R.id.radioGroupColor);
+        swNotaFavorita = view.findViewById(R.id.switchNotaFavorita);
+
         builder.setView(view);
         // Create the AlertDialog object and return it
         return builder.create();
